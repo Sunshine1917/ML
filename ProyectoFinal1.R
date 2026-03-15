41c67a37a5dde579299eeda27a5868df0b0d937e
System Administrator <root@MacBook-Air.local>
2026-03-15 16:39:11 -0600

    In this just load the insurance.csv and watch the variables, I need do the extensive KDD

---
+++ ProyectoFinal1.R
@@ -0,0 +1,8 @@
+# ======= PROYECTO FINAL ======= #
+# Alejandro Juárez Rojas 
+setwd("/Users/alejandrojuarezrojas/Downloads/ML")
+getwd() #Cambiamos al directorio donde esta nuestro dataset
+
+insurace <- read.csv("insurance.csv") 
+str(insurace) #Vemos la esructura de datos, dimensión y variables
+
