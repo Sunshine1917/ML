a2725ae19e93a3387ff59f46a2701fb5b9548bff
Sunshine1917 <alejandro.juarez.120104@gmail.com>
2026-03-15 20:35:50 -0600

    update files, I'm cheaking the outliers

--- ProyectoFinal1.R
+++ ProyectoFinal1.R
@@ -4,5 +4,19 @@ setwd("/Users/alejandrojuarezrojas/Downloads/ML")
 getwd() #Cambiamos al directorio donde esta nuestro dataset
 
 insurace <- read.csv("insurance.csv") 
-str(insurace) #Vemos la esructura de datos, dimensión y variables
 
+# ======= Inspección inicial ======= #
+str(insurace) #Vemos la esructura de datos, dimensión y variables
+summary(insurace)
+
+# Observamos si no contamos con valores NA en el data set
+table(is.na(insurace))
+colSums(is.na(insurace))
+
+# Vemos si no tenemos valores duplicados
+sum(duplicated(insurace))
+insurace <- insurace[!duplicated(insurace),] #Se detecto un duplicado por lo que lo eliminamos
+
+#Para detectar valores atípicos que puedan distorcionar el modelo 
+par(mrfrow = c(2,2))
+boxplot(insurace$age, main = "Age", col = "#2E75B6", ylab = "Value")
